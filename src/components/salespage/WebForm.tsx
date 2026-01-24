dcb42714b76609880c7f548413c8b898fee510bb
dev1IREG61 <ireg.member61@gmail.com>
2026-01-24 18:16:55 +0500

    Added pr and cs

--- src/components/salespage/WebForm.tsx
+++ src/components/salespage/WebForm.tsx
@@ -427,10 +427,7 @@ const WebForm: React.FC<WebFormProps> = ({ isOpen, onClose, data }) => {
               onClick={onClose}
               className="absolute top-4 right-4 z-10 p-2 hover:bg-gray-100 rounded-full transition-colors"
             >
-              <EasyIcon
-                iconPath="feather/FiX"
-                className="w-5 h-5 text-gray-600"
-              />
+              <EasyIcon iconPath="feather/FiX" className="w-5 h-5 text-white" />
             </button>
 
             {showSuccess ? (
